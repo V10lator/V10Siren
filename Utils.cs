d14dea5768fdc80ac0f93e04d667170c89fd323f
Thomas Rohloff <v10lator@myway.de>
2018-06-21 12:51:01 +0200

    Add a License WARNING: Before this commit all code was proprietary!
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- Utils.cs
+++ Utils.cs
@@ -1,4 +1,22 @@
 /*
+ * This file is part of V10Siren.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * V10Siren is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with V10Siren.  If not, see
+ * <http://www.gnu.org/licenses/>.
+/*
+
+/*
  * Don't copy this file to your mod!
  * It might get splitted into a library later but right now
  * copying it is a save way to end the world as we know it.
--- V10Siren.cs
+++ V10Siren.cs
@@ -1,3 +1,21 @@
+/*
+ * This file is part of V10Siren.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * V10Siren is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with V10Siren.  If not, see
+ * <http://www.gnu.org/licenses/>.
+/*
+
 using ColossalFramework.IO;
 using ColossalFramework.Steamworks;
 using ICities;
