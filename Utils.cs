ce3a336e4cb0671aa4a96d65e40a62f8ad93f5c4
Thomas Rohloff <v10lator@myway.de>
2018-06-21 15:31:50 +0200

    Fix d14dea5768fdc80ac0f93e04d667170c89fd323f
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- Utils.cs
+++ Utils.cs
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General
  * Public License along with V10Siren.  If not, see
  * <http://www.gnu.org/licenses/>.
-/*
+ */
 
 /*
  * Don't copy this file to your mod!
--- V10Siren.cs
+++ V10Siren.cs
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General
  * Public License along with V10Siren.  If not, see
  * <http://www.gnu.org/licenses/>.
-/*
+ */
 
 using ColossalFramework.IO;
 using ColossalFramework.Steamworks;
